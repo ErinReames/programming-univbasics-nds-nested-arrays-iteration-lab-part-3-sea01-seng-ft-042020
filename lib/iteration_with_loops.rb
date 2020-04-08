0303c4763d43d91ab65721cad1ad5d439f3fcb5f
Erin Reames <italiagirl83@gmail.com>
2020-04-08 22:29:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,20 +1,19 @@
 def join_nested_strings(src)
-  outer_results = []
+  sentence = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    # How to read the following line of code:
     #   Array at row_index
     #   Element of the inner array at element_index
     #   The first character of that element...
+    
     if src[row_index][element_index] === String.class
-      outer_results << src[row_index][element_index]
+      sentence << src[row_index][element_index]
     end
     element_index += 1
   end
   row_index += 1
 end
- 
-outer_results.join("")
+sentence.join("")
 end
