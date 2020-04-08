256e82953c231c9ff767971acd51751dfad9fcdc
Erin Reames <italiagirl83@gmail.com>
2020-04-08 22:27:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
     #   Array at row_index
     #   Element of the inner array at element_index
     #   The first character of that element...
-    if src[row_index][element_index][0] == String.class
+    if src[row_index][element_index] === String.class
       outer_results << src[row_index][element_index]
     end
     element_index += 1
